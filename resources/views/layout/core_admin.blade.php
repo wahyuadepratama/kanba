87af270fa96c7d0d6a291a143d786138120b7ab8
wahyuadepratama <wahyu.mailist@gmail.com>
2019-09-10 00:02:04 +0700

    repair icon

--- resources/views/layout/core_admin.blade.php
+++ resources/views/layout/core_admin.blade.php
@@ -110,7 +110,7 @@
             <li class="nav-item dropdown no-arrow" >
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                  <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ session('login')->name }}</span>
-                  <img class="img-profile rounded-circle" src="{{ asset('img/man-user.svg') }}">
+                  <img class="img-profile rounded-circle" src="{{ asset('/img/user.jpg') }}">
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
